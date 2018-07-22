2bd1202faa89b2ab64b17ffc5805c1c6a2dc1b2e
Megan Quinn <mnquinn@gmail.com>
2018-07-22 16:36:55 -0500

    Help from Lou in getting cards to display

--- js/app.js
+++ js/app.js
@@ -2,16 +2,17 @@
  * Create a list that holds all of your cards
  */
 const cardList = [
-     "/img/ichi.jpg", "/img/ni.jpg", "/img/san.jpg", "/img/shi.jpg",
-	"/img/go.jpg", "/img/roku.jpg", "/img/nana.jpg", "/img/hachi.jpg",
-     "/img/ichi.jpg", "/img/ni.jpg", "/img/san.jpg", "/img/shi.jpg",
-	"/img/go.jpg", "/img/roku.jpg", "/img/nana.jpg", "/img/hachi.jpg"
+     "img/ichi.jpg", "img/ni.jpg", "img/san.jpg", "img/shi.jpg",
+	"img/go.jpg", "img/roku.jpg", "img/nana.jpg", "img/hachi.jpg",
+     "img/ichi.jpg", "img/ni.jpg", "img/san.jpg", "img/shi.jpg",
+	"img/go.jpg", "img/roku.jpg", "img/nana.jpg", "img/hachi.jpg"
 ]; // Creates array with all of my cards (listed twice so they have pairs)
 const deck = document.getElementById('deck'); // Targets the ul list that will hold cards
 const newBoard = document.getElementById('reset'); // Targets the New Board/reset button
 let openedCards = []; // Creates an open array for cards to be placed to compare matches
 let moveCount = 0; // Creates a running counter for moves made in the game
-let trueCardArr = [];
+let matchCount = 0; //Creates a running counter for matches made in the game
+let trueCardArr = []; // Creates array to hold generated html text
 
 /*
  * Display the cards on the page
@@ -47,29 +48,33 @@ function createCardList(card) {
 
 // Put the shuffled, created cards onto the board
 function createGameBoard() {
-	const cardHTML = cardList.map(function(card) {
-		return cardList(card);
-	});
-	deck.innerHTML = cardHTML.join('');
+     // ForEach method called on trueCardArr adds each card into the deck, returning the entire deck at the end
+	trueCardArr.forEach(function(card) {
+          return deck.innerHTML += card;
+     });
 	return deck;
-}
+};
 
- // Initial load of cards after page loads
+// Initial load of cards after page loads
 function startGame() {
-     shuffle(cardList);
      cardList.forEach(function(card) {
           trueCardArr.push(createCardList(card));
      });
+     shuffle(trueCardArr);
 	createGameBoard();
 	// Initialize clock
 };
 
-// Resets the board when the "New Board" button is clicked
-newBoard.addEventListener('click', function() {
-     //TODO: Add an if statement with listeners to prompt user if they really want to restart game.
-	openedCards = [];
-	startGame();
-});
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
 
 // Base listener function on deck to flip cards and check matches
 deck.addEventListener('click', function(evt) {
@@ -105,25 +110,22 @@ function checkMatch(clickedCard) {
           makeMatch(openedCards);
      } else {
           clearOpened(openedCards);
-     }
-}
+     };
+};
 
 // Adds the match class to 2 cards if they have matched, checks for all cards matched
 function makeMatch(openedCards) {
 	openedCards[1].classList.add('match');
 	openedCards[0].classList.add('match');
-	if (moveCount === 16) {
+     matchCount += 1;
+	if (matchCount === 8) {
 		//end game: stop timer, pop-up with stats
-	}
-}
+	};
+};
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Resets the board when the "New Board" button is clicked
+newBoard.addEventListener('click', function() {
+    //TODO: Add an if statement with listeners to prompt user if they really want to restart game.
+    openedCards = [];
+    startGame();
+});
